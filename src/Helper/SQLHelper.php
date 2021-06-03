25f1b470fee4535bf8528d44f5a85a4f582b9b2a
zuowb <20021575@qq.com>
2021-06-03 15:38:58 +0800

    fix logsql()

--- src/Helper/SQLHelper.php
+++ src/Helper/SQLHelper.php
@@ -14,16 +14,18 @@ class SQLHelper
         DB::listen(function ($query) {
             $tmp = str_replace('?', '"' . '%s' . '"', $query->sql);
             $qBindings = [];
-            foreach ($query->bindings as $key => $value) {
-                if (is_numeric($key)) {
-                    $qBindings[] = $value;
-                } else {
-                    $tmp = str_replace(':' . $key, '"' . $value . '"', $tmp);
+            if (!empty($query->bindings)) {
+                foreach ($query->bindings as $key => $value) {
+                    if (is_numeric($key)) {
+                        $qBindings[] = $value;
+                    } else {
+                        $tmp = str_replace(':' . $key, '"' . $value . '"', $tmp);
+                    }
                 }
+                $tmp = vsprintf($tmp, $qBindings);
+                $tmp = str_replace("\\", "", $tmp);
+                Log::info(' execution time: ' . $query->time . 'ms; ' . $tmp . "\n\n\t");
             }
-            $tmp = vsprintf($tmp, $qBindings);
-            $tmp = str_replace("\\", "", $tmp);
-            Log::info(' execution time: ' . $query->time . 'ms; ' . $tmp . "\n\n\t");
         });
     }
 }
